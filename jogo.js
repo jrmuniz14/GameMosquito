dede9975bea322402d3325a4b6d753c350decda6
Junio <junio@LAPTOP-E5PMT191>
2022-02-01 21:06:07 -0300

    parte inicial do game

---
+++ jogo.js
@@ -0,0 +1,82 @@
+
+var altura = 0
+var largura = 0
+
+var posX = 0
+var posY = 0
+
+function getTamanhoTela(){
+	
+	altura = window.innerHeight
+	largura = window.innerWidth
+}
+
+
+function createRadomPosition(){
+	posX = Math.floor(Math.random() * largura) -120
+
+	if (posX < 0) {
+		posX = posX + 120
+	}
+
+	if ((posX+120) > largura) {
+		posX = posX - 120
+	}
+
+	posY = Math.floor(Math.random() * altura) -120
+
+	if (posY < 0) {
+		posY = posY + 120
+	}
+
+	if ((posY+120) > largura) {
+		posY = posY - 120
+	}
+}
+
+
+function addMosca(){
+
+	if(document.getElementById('mosquito')){
+		document.getElementById('mosquito').remove()
+	}
+
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosca.png'
+	mosquito.className = tamanhoAleatorio() +' '+ ladoAleatorio()
+
+	mosquito.style.position = 'absolute'
+	mosquito.style.left = posX +'px'
+	mosquito.style.top = posY +'px'
+
+	mosquito.id = 'mosquito'
+
+	document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio(){
+	var classe = Math.floor(Math.random()*3)
+
+	switch(classe){
+		case 0:
+			return 'mosquito0'
+
+		case 1:
+			return 'mosquito1'
+
+		case 2:
+			return 'mosquito2'
+	}
+}
+
+function ladoAleatorio(){
+	var classe = Math.floor(Math.random()*2)
+
+	switch(classe){
+		case 0:
+			return 'ladoA'
+
+		case 1:
+			return 'ladoB'
+	}
+}
